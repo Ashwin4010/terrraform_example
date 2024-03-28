5762fd2dd3172a05788e792eedb7fb8252593fb5
Ashwin4010 <ashwinpremnath123@gmail.com>
2024-03-28 20:44:32 +0530

    Update ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -15,6 +15,8 @@ resource "aws_instance" "Terraform_instance" {
   instance_type = "t2.micro"
   subnet_id     = "subnet-0fc24d3949e171bcf"
   security_groups = ["sg-0e272864109ba6399"]
+  key_name = "jenkins_2"
+  count = "1"
 
   tags = {
     Name = "my_Terra_instance"
