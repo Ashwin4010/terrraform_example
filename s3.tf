156422423e9e5d51c8eadd9ca03215aad3a7b8e5
ashwin4010 <ashwinpremnath123@gmail.com>
2024-03-28 06:32:27 +0000

    update: terraform s3 bucket code update

--- s3.tf
+++ s3.tf
@@ -1,1 +1,20 @@
-hello
+terraform {
+    required_providers {
+      aws = {
+        source  = "hashicorp/aws"
+        version = "5.19.0"  # Use the latest version
+      }
+    }
+  }
+provider "aws" {
+    region = "ap-south-1"  # Replace with your desired AWS region
+  }
+
+resource "aws_s3_bucket" "terra_ashwin_sample" {
+  bucket = "my-tf-test-bucket"
+
+  tags = {
+    Name        = "My bucket"
+    Environment = "Dev"
+  }
+}
